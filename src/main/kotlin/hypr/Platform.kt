ed4e73a8ffaf567eefa74c1c1712254697fd836b
Martyn Garcia <martyn@255bits.com>
2017-08-10 23:39:00 -0600

    [feature]  couch integration

--- src/main/kotlin/hypr/Platform.kt
+++ src/main/kotlin/hypr/Platform.kt
@@ -9,9 +9,20 @@ import com.squareup.moshi.*
 import spark.TemplateEngine
 import spark.template.velocity.VelocityTemplateEngine
 
+import org.ektorp.*
+import org.ektorp.http.*
+import org.ektorp.impl.*
+import org.ektorp.support.*
+import com.fasterxml.jackson.annotation.*
+
 val logger: Logger = LoggerFactory.getLogger("main")
 
-data class Generator(val name: String) 
+@JsonIgnoreProperties("id", "revision")
+data class Generator(
+  @JsonProperty("name") var name: String, 
+  @JsonProperty("_id") var id:String,
+  @JsonProperty("_rev") var revision: String? = null
+) 
 fun main(args: Array<String>) {
 
     val application = Platform(8080)
@@ -19,20 +30,34 @@ fun main(args: Array<String>) {
 }
 
 
+class GeneratorRepository(db: CouchDbConnector) : CouchDbRepositorySupport<Generator>(Generator::class.java, db) {
+}
+
 class Platform(val port: Int = 8080) {
 
     val renderEngine: TemplateEngine
         get() = VelocityTemplateEngine()
 
     fun start() {
-      
+        val httpClient:HttpClient = StdHttpClient.Builder().url("http://localhost:5984").build()
+
+        val dbInstance:CouchDbInstance  = StdCouchDbInstance(httpClient)
+        val db:CouchDbConnector = StdCouchDbConnector("generators", dbInstance)
+
+        var repo :GeneratorRepository = GeneratorRepository(db)
+
+        db.createDatabaseIfNotExists()
+
         port(8080)
 
         get("/v1/models.json") { req, res ->
           res.type("application/json")
-          val results = ArrayList<Generator>()
-          results.add(Generator("TEST"))
+          val results = repo.getAll()
           Gson().toJson(results)
+        }
+        post("/v1/models.json") { req, res ->
+          res.type("application/json")
+          repo.add(Generator("Test", "test"))
         }
     }
 
